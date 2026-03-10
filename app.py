39d9c53d79b7ea567ce06dc055eba8a7eb4d6b56
Ramana1205 <gorlaramana05@gmail.com>
2026-03-10 12:15:16 +0530

    deploy flask app

--- app.py
+++ app.py
@@ -68,5 +68,5 @@ def _game_result(board):
         return 'white_wins' if board.turn == chess.BLACK else 'black_wins'
     return 'draw'
 
-if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
